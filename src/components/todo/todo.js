65a78ed832b30909b9d23bc1834feba6470203a8
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 00:08:49 -0700

    Added context usage, componentized elements

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,35 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form.js';
+import React, { useContext, useEffect, useState } from 'react';
+import TodoItem from '../todoItem/todoItem';
 
-import { v4 as uuid } from 'uuid';
+import { Menu, Button, ButtonGroup } from '@blueprintjs/core';
+import { SettingsContext } from '../../context/settings.js';
+import Form from '../form/form';
 
 const ToDo = () => {
 
-  const defaultValues = {
-    difficulty: 4,
-  }
-  
+  const { showCompleted, itemQty, sortParams } = useContext(SettingsContext)
   const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
-  const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
-
-  function addItem(item) {
-    console.log(item);
-    item.id = uuid();
-    item.complete = false;
-    setList([...list, item]);
-  }
+  const [page, setPage] = useState(1);
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter(item => item.id !== id);
     setList(items);
+  }
+
+  function addToList(item) {
+    setList([...list, item]);
   }
 
   function toggleComplete(id) {
 
-    const items = list.map( item => {
-      if ( item.id == id ) {
-        item.complete = ! item.complete;
+    const items = list.map(item => {
+      if (item.id === id) {
+        item.complete = !item.complete;
       }
       return item;
     });
@@ -38,52 +34,55 @@ const ToDo = () => {
 
   }
 
+  function changePage(e) {
+    e.preventDefault();
+    setPage(parseInt(e.target.value));
+  }
+
   useEffect(() => {
     let incompleteCount = list.filter(item => !item.complete).length;
     setIncomplete(incompleteCount);
     document.title = `To Do List: ${incomplete}`;
-  }, [list]);
+  }, [list, incomplete]);
+
+  let sortedList = [...list];
+  if (!showCompleted) {
+    sortedList = sortedList.filter((item) => !item.complete)
+  }
+
+  sortedList = sortedList.filter((item) => {
+    console.log(item.text, sortParams)
+    return item.text.toLowerCase().includes(sortParams.toLowerCase());
+  });
+
+  let pageButtons = [];
+  let pageQty = Math.ceil(sortedList.length / itemQty);
+  for (let i = 1; i <= pageQty; i++) {
+    pageButtons[i] = <Button onClick={changePage} value={i}>{i}</Button>
+  }
+
+  sortedList = sortedList.filter((item, idx) => idx < page * itemQty && idx >= (page - 1) * itemQty)
 
   return (
     <>
       <header>
         <h1>To Do List: {incomplete} items pending</h1>
       </header>
-
-      <form onSubmit={handleSubmit}>
-
-        <h2>Add To Do Item</h2>
-
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
-
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
-
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
-        </label>
-
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
-
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
-
+      <Menu>
+        <Form addToList={addToList}/>
+        <ButtonGroup>
+          {pageButtons}
+        </ButtonGroup>
+      </Menu>
+      <div>
+        {sortedList.map(item => (
+          <TodoItem
+            key={item.id}
+            toggleComplete={toggleComplete}
+            item={item}
+          />
+        ))}
+      </div>
     </>
   );
 };
