9f176c2456bf9207a6996a0d394f2bf3ddb87800
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 00:08:59 -0700

    Created todo list item

---
+++ src/components/todoItem/todoItem.js
@@ -0,0 +1,15 @@
+import { Card, Checkbox } from "@blueprintjs/core";
+
+
+function TodoItem({item, toggleComplete}) {
+  return( 
+    <Card>
+      <p>{item.text}</p>
+      <p><small>Assigned to: {item.assignee}</small></p>
+      <p><small>Difficulty: {item.difficulty}</small></p>
+      <Checkbox onClick={() => toggleComplete(item.id)}><small>Complete</small></Checkbox>
+    </Card>
+  );
+}
+
+export default TodoItem;
