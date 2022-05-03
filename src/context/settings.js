7373e668cae112b477d8333cfa652b4596310036
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 00:09:12 -0700

    Created settings context file

---
+++ src/context/settings.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export const SettingsContext = React.createContext();
+
+function Settings({ children }) {
+  const [showCompleted, setShowCompleted] = useState(true);
+  const [itemQty, setItemQty] = useState(6);
+  const [sortParams, setSortParams] = useState('henlo');
+
+  return (
+    <SettingsContext.Provider value={{showCompleted, itemQty, sortParams}}>
+      {children}
+    </SettingsContext.Provider>
+  );
+}
+
+export default Settings;
