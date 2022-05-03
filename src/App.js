b1ab3de1de2b4da73eed7bf42c85aa87ab260f16
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 11:11:31 -0700

    Refactored App to be a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
 import ToDo from './components/todo/ToDoList.js';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <ToDo />
-    );
-  }
+function App() {
+  return (
+    <ToDo />
+  );
 }
+
+export default App;
