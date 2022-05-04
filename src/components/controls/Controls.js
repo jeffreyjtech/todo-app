57f46b9a28d8b7b132309087f95806ffa003488b
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 20:08:55 -0700

    Created blank Controls component

---
+++ src/components/controls/Controls.js
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+
+import { SettingsContext } from '../../context/settings.js';
+
+function Controls() {
+  
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default Controls;
