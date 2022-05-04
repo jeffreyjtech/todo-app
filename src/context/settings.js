d4f078e5ccfb71b24eb3e1d356db2ed291f66c6e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-03 20:08:18 -0700

    Added setters wrappers to settings context

--- src/context/settings.js
+++ src/context/settings.js
@@ -3,12 +3,36 @@ import React, { useState } from "react";
 export const SettingsContext = React.createContext();
 
 function Settings({ children }) {
-  const [showCompleted, /*setShowCompleted*/] = useState(true);
-  const [itemQty, /*setItemQty*/] = useState(6);
-  const [sortParams, /*setSortParams*/] = useState('');
+  const [showCompleted, setShowCompleted] = useState(true);
+  const [itemQty, setItemQty] = useState(6);
+  const [sortParams, setSortParams] = useState('');
 
+  const handleSortParams = (params) => {
+    setSortParams(params.toString());
+  }
+
+  const handleItemQty = (qty) => {
+    const parsedQty = parseInt(qty)
+    if(!parsedQty.isNaN()) {
+      setItemQty(parsedQty);
+    }
+  }
+
+  const toggleShowCompleted = () => {
+    setShowCompleted(!showCompleted);
+  }
+
+  const value = { 
+    showCompleted,
+    toggleShowCompleted, 
+    itemQty,
+    handleItemQty, 
+    sortParams,
+    handleSortParams, 
+  }
+  
   return (
-    <SettingsContext.Provider value={{ showCompleted, itemQty, sortParams }}>
+    <SettingsContext.Provider value={value}>
       {children}
     </SettingsContext.Provider>
   );
