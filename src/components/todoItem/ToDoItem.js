e821c6f6017785ba62ffd756f1a334acb5511e23
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-04 21:12:56 -0700

    Added update capability check to Checkbox

--- src/components/todoItem/ToDoItem.js
+++ src/components/todoItem/ToDoItem.js
@@ -1,18 +1,23 @@
 import { Card, Checkbox } from "@blueprintjs/core";
 
+import Auth from "../auth/Auth";
+
 function TodoItem({ item, toggleComplete }) {
+
   return (
     <Card>
       <p>{item.text}</p>
       <p><small>Assigned to: {item.assignee}</small></p>
       <p><small>Difficulty: {item.difficulty}</small></p>
-      <Checkbox 
-        checked={item.complete} 
-        onClick={() => toggleComplete(item.id)} 
-        data-testid="checkbox"
-      >
-        <small>Complete</small>
-      </Checkbox>
+      <Auth capability="update">
+        <Checkbox 
+          checked={item.complete} 
+          onClick={() => toggleComplete(item.id)} 
+          data-testid="checkbox"
+        >
+          <small>Complete</small>
+        </Checkbox>
+      </Auth>
     </Card>
   );
 }
