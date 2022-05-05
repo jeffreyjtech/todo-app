2ff61cdfc49a2801f79bcb705a5285192e5ff810
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-04 21:55:46 -0700

    Renamed AuthProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginContext from './context/auth';
+import AuthProvider from './context/auth';
 import './index.scss'
 import App from './App.js';
 import SettingsProvider from './context/settings';
@@ -8,10 +8,10 @@ import SettingsProvider from './context/settings';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginContext>    
+    <AuthProvider>    
       <SettingsProvider>
         <App />
       </SettingsProvider>
-    </LoginContext>
+    </AuthProvider>
   </React.StrictMode>
 );
