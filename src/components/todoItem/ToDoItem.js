09bc4a9b5d0cebefca2c0fd5894372484ef23a32
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-04 21:56:28 -0700

    Added delete auth check to Delete button

--- src/components/todoItem/ToDoItem.js
+++ src/components/todoItem/ToDoItem.js
@@ -1,19 +1,26 @@
-import { Card, Checkbox } from "@blueprintjs/core";
+import { Button, Card, Checkbox, Icon } from "@blueprintjs/core";
+
+import './toDoItem.scss';
 
 import Auth from "../auth/Auth";
 
-function TodoItem({ item, toggleComplete }) {
+function TodoItem({ item, toggleComplete, deleteItem }) {
 
   return (
     <Card>
       <p>{item.text}</p>
       <p><small>Assigned to: {item.assignee}</small></p>
       <p><small>Difficulty: {item.difficulty}</small></p>
+      <Auth capability="delete">
+        <Button onClick={() => deleteItem(item.id)}>
+          <Icon icon="delete" intent="Danger"/>
+        </Button>
+      </Auth>
       <Auth capability="update">
         <Checkbox 
           checked={item.complete} 
           onClick={() => toggleComplete(item.id)} 
-          data-testid="checkbox"
+          data-testid={`checkbox-${item.text}`}
         >
           <small>Complete</small>
         </Checkbox>
