ffa0b3bf79ef22cf7535a456c42d1c572f2351c9
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-06 00:33:05 -0700

    Disable spellcheck on tokens

--- src/context/auth.js
+++ src/context/auth.js
@@ -8,25 +8,25 @@ const testUsers = {
   Administrator: {
     password: 'admin',
     name: 'Administrator',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW5pc3RyYXRvciIsInJvbGUiOiJhZG1pbiIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJywncmVhZCcsJ3VwZGF0ZScsJ2RlbGV0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.pAZXAlTmC8fPELk2xHEaP1mUhR8egg9TH5rCyqZhZkQ'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW5pc3RyYXRvciIsInJvbGUiOiJhZG1pbiIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJywncmVhZCcsJ3VwZGF0ZScsJ2RlbGV0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.pAZXAlTmC8fPELk2xHEaP1mUhR8egg9TH5rCyqZhZkQ' /* cspell: disable-line */
   },
   Reader: {
     password: 'reader',
     name: 'Reader',
     capabilities: ['read'],
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUmVhZGVyIiwiY2FwYWJpbGl0aWVzIjpbInJlYWQiXX0.8jtDua75M64EiablS8CJIaD4KV9rMzLFGaKXsbEybco'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUmVhZGVyIiwiY2FwYWJpbGl0aWVzIjpbInJlYWQiXX0.8jtDua75M64EiablS8CJIaD4KV9rMzLFGaKXsbEybco' /* cspell: disable-line */
   },
   WriteOnly: {
     password: 'writeonly',
     name: 'WriteOnly',
     capabilities: ['read', 'write'],
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVPbmx5IiwiY2FwYWJpbGl0aWVzIjpbInJlYWQiLCJ3cml0ZSJdfQ.gGOiubOphgkdA8xJdEmgINV6hV530_gksx9PbXUKtGY'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVPbmx5IiwiY2FwYWJpbGl0aWVzIjpbInJlYWQiLCJ3cml0ZSJdfQ.gGOiubOphgkdA8xJdEmgINV6hV530_gksx9PbXUKtGY' /* cspell: disable-line */
   },
   Writer: {
     password: 'writer',
     name: 'Writer',
     capabilities: ['read', 'write', 'update', 'delete'],
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVyIiwiY2FwYWJpbGl0aWVzIjpbInJlYWQiLCJ3cml0ZSIsInVwZGF0ZSIsImRlbGV0ZSJdfQ.jrb7yupzGaUIJcDX-CQs93zEl6czjdOZyoHFcqiYpuk'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVyIiwiY2FwYWJpbGl0aWVzIjpbInJlYWQiLCJ3cml0ZSIsInVwZGF0ZSIsImRlbGV0ZSJdfQ.jrb7yupzGaUIJcDX-CQs93zEl6czjdOZyoHFcqiYpuk' /* cspell: disable-line */
   },
 }
 
