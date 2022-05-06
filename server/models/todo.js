53a1aa889d272e1ccf1ae0b3522e2f8fe2c7a152
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-06 00:20:34 -0700

    Allowed null for assignee and fixed defaultValue property

--- server/models/todo.js
+++ server/models/todo.js
@@ -4,8 +4,8 @@ const todoModel = (sequelize, DataTypes) => {
   const model = sequelize.define('todo', {
     author: { type: DataTypes.STRING, allowNull: false },
     text: { type: DataTypes.STRING, allowNull: false },
-    assignee: { type: DataTypes.STRING, allowNull: false },
-    complete: { type: DataTypes.BOOLEAN, default: false, allowNull: false },
+    assignee: { type: DataTypes.STRING, allowNull: true },
+    complete: { type: DataTypes.BOOLEAN, defaultValue: false, allowNull: false },
   });
 
   return model;
